a57c787f1b7aa84ad8fa98689193511e254c5611
Nithishdking <nithishdking@gmail.com>
2024-08-05 17:47:19 +0530

    added

---
+++ Randompasswordgenerator.py
@@ -0,0 +1,31 @@
+import string #importing string to declare all type of string 
+import random # importing random to randomly it can declare or assgin 
+
+Characters = list(string.ascii_letters+"!@#$%^&*()"+string.digits)
+
+def password_generator():
+    pass_len = int(input("Give a length of a password: "))
+
+    random.shuffle(Characters) #it shuffle the value of character
+    password = []
+
+    for x in range(pass_len):
+        password.append(random.choice(Characters))
+
+    random.shuffle(password) # it shuffle the password value
+
+    password = "" .join(password)
+    print(password)
+
+option = input("Do you want to generate a password automatically ?  (Yes/No): ")
+
+if option == "Yes" or "yes":
+    password_generator()
+
+elif option == "No" or"no":
+    print("Thank you!")
+    quit()
+else:
+    print("Invalid input, please enter Yes or No")
+    quit()
+
